455b02270e7da426c7d07d90c7220771c0fead88
Sergey Baldin <fesswoodwork@gmail.com>
2024-01-18 11:18:06 +0700

    added wrk script

--- src/main/kotlin/com/github/sbaldin/warmup/demospring/warmup/WarmupService.kt
+++ src/main/kotlin/com/github/sbaldin/warmup/demospring/warmup/WarmupService.kt
@@ -1,10 +1,32 @@
 package com.github.sbaldin.warmup.demospring.warmup
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+
+val logger = LoggerFactory.getLogger(WarmupService::class.java)
 
 @Component
 class WarmupService {
     fun warmup() {
-        TODO("Not yet implemented")
+        logger.infoStartFinish("warmup") {
+            warmupMethod()
+        }
+    }
+
+    private fun warmupMethod() {
+        logger.info("warmup method is call.")
     }
 }
+
+private fun Logger.infoStartFinish(mark: String, fn: () -> Unit) {
+    val started = System.nanoTime()
+    info("Started: $mark.")
+    val finished = System.nanoTime()
+    val durationInMs = TimeUnit.NANOSECONDS.toMillis(finished - started)
+    info("Finished: $mark in ${durationInMs} ms.")
+}
+
+
